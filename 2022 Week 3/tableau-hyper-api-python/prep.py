32bf052f9f08dafb37ae7a3f64844308c43d639b
Jeremy Harris <jercharris89@gmail.com>
2022-01-19 22:18:16 -0800

    add comments to python code

--- 2022 Week 3/tableau-hyper-api-python/prep.py
+++ 2022 Week 3/tableau-hyper-api-python/prep.py
@@ -3,11 +3,13 @@ from tableauhyperapi import HyperProcess, Connection, CreateMode, TableDefinitio
     Telemetry
 
 
+# start hyper process and open connection to database
 with HyperProcess(Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
     with Connection(
         endpoint=hyper.endpoint, database='2022-3-output.hyper', create_mode=CreateMode.CREATE_AND_REPLACE
     ) as connection:
 
+        # create roster table definition object
         roster_def = TableDefinition('roster', [
             TableDefinition.Column('id', SqlType.int()),
             TableDefinition.Column('pupil first name', SqlType.text()),
@@ -20,6 +22,7 @@ with HyperProcess(Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
             TableDefinition.Column('Parental Contact', SqlType.int())
         ])
 
+        # create grades table definition object
         grades_def = TableDefinition('grades', [
             TableDefinition.Column('Student ID', SqlType.int()),
             TableDefinition.Column('Maths', SqlType.int()),
@@ -31,23 +34,28 @@ with HyperProcess(Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
             TableDefinition.Column('Geography', SqlType.int())
         ])
 
+        # create hyper tables from table definitions
         connection.catalog.create_table(roster_def)
         connection.catalog.create_table(grades_def)
 
+        # copy class roster csv to roster table sql
         copy_roster = (
             f"COPY {roster_def.table_name} "
             f"FROM {escape_string_literal(os.path.join('..', 'PD 2022 Wk 3 Input.csv'))} "
             f"WITH (FORMAT CSV, NULL 'NULL', delimiter ',', header)"
         )
 
+        # copy grades csv to grades table sql
         copy_grades = (
             f"COPY {grades_def.table_name} "
             f"FROM {escape_string_literal(os.path.join('..', 'PD 2022 WK 3 Grades.csv'))} "
             f"WITH (FORMAT CSV, NULL 'NULL', delimiter ',', header)"
         )
 
+        # execute both copy sql statements
         connection.execute_command(copy_roster)
         connection.execute_command(copy_grades)
 
+        # create output table using create table as plus sql data prep logic from prep.sql to shape data
         sql = 'create table "output" as ' + open('prep.sql', 'r').read()
         connection.execute_command(sql)

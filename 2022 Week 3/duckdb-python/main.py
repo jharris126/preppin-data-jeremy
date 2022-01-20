54f5568cf9f523688511d8b153906487b492f9d3
Jeremy Harris <jercharris89@gmail.com>
2022-01-19 20:13:32 -0800

    make less pythonic but more beginner friendly
    remove functions and main() starting point in favor of top to bottom data prep script, more friendly to python beginners

--- 2022 Week 3/duckdb-python/main.py
+++ 2022 Week 3/duckdb-python/main.py
@@ -2,46 +2,27 @@ import duckdb
 import os
 
 
-# loads csv data files form datasets variable and inserts into a table named whatever string is in dataset table key
-def load_to_table():
-    # sql to load the file into a table and duckdb command to execute it
-    for dataset in datasets:
-        cta_sql = f"create table {dataset['table']} as select * from read_csv_auto('{dataset['file']}');"
-        conn.execute(cta_sql)
+# tables to be created in duckdb from csv
+datasets = [
+    {'file': os.path.join('..', 'PD 2022 Wk 3 Input.csv'), 'table': 'roster'},
+    {'file': os.path.join('..', 'PD 2022 WK 3 Grades.csv'), 'table': 'grades'}
+]
 
+# create a duckdb connection object from which all duckdb queries and commands will be executed
+conn = duckdb.connect()
 
-def transform_and_write():
-    # sql to perform the data prep, sql now stored in separate prep.sql file in this directory for improved clarity
-    sql = open('prep.sql', 'r').read()
+# generate sql to load the files into tables and duckdb command to execute it
+for dataset in datasets:
+    cta_sql = f"create table {dataset['table']} as select * from read_csv_auto('{dataset['file']}');"
+    conn.execute(cta_sql)
 
-    # print tuples for testing
-    # print(conn.execute(sql).fetchall())
+# sql to perform the data prep, sql now stored in separate prep.sql file in this directory for improved clarity
+sql = open('prep.sql', 'r').read()
 
-    # define output file name/path, sql to execute and export, and duckdb command to execute the sql
-    outfile = '2022-3-output.csv'
-    export_sql = f"copy ({sql}) to '{outfile}' with (header 1, delimiter ',');"
-    conn.execute(export_sql)
+# print list of tuples for testing prep.sql output
+# print(conn.execute(sql).fetchall())
 
-
-# main function, best practice in python for creating a starting point and context for beginning code execution
-if __name__ == '__main__':
-    # tables to be created in duckdb from csv
-    datasets = [
-        {
-            'file': os.path.join('..', 'PD 2022 Wk 3 Input.csv'),
-            'table': 'roster'
-        },
-        {
-            'file': os.path.join('..', 'PD 2022 WK 3 Grades.csv'),
-            'table': 'grades'
-        }
-    ]
-
-    # create a duckdb connection object from which all duckdb queries and commands will be executed
-    conn = duckdb.connect()
-
-    # invoke function to load input csv data to a duckdb table
-    load_to_table()
-
-    # invoke function to transform (prep) the data and export as output csv
-    transform_and_write()
+# define output file name/path, sql to execute and export, and duckdb command to execute the sql
+outfile = '2022-3-output.csv'
+export_sql = f"copy ({sql}) to '{outfile}' with (header 1, delimiter ',');"
+conn.execute(export_sql)
